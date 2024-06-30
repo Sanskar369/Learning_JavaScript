97463cdcb1c74d7bed52630e723a898ba0d707f6
Sanskar369 <109854095+Sanskar369@users.noreply.github.com>
2024-06-30 15:57:21 +0000

    Datatype Summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,31 @@
+// primitive
+
+// 7 types : String,Number,Boolean,null,undefined,symbol,BigInt
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+const bigNumber = 3265616515151515156n
+
+
+// Reference(Non Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["Shaktiman","naagraj","dogga"];
+let myObj={
+    name: "Sanskar",
+    age: 21,
+}
+
+const myfunction =function(){
+    console.log("Hello world");
+}
+
+console
